fca5c5ceed6917986449d0c571d5c8db1f9662e6
alex <ryunenln@gmail.com>
2026-04-09 18:55:45 +0800

    添加思源黑体作为中文字符

--- src/config.ts
+++ src/config.ts
@@ -219,9 +219,9 @@ export const siteConfig: SiteConfig = {
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "萝莉体 第二版",
+			fontFamily: "思源黑体",
 			fontWeight: "500",
-			localFonts: ["loli.ttf"],
+			localFonts: ["SourceHanSansCN-VF.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 	},
